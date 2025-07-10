8b17c851e434734e925907c339b0fdc97e9eee24
Anurag Pal <apal895@gmail.com>
2025-07-10 12:35:15 +0530

    added upload and middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,44 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "https://cecs-s3.s3.ap-south-1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "https://cecs-s3.s3.ap-south-1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+      cors: {
+        origin: [
+          "http://localhost:3000",
+          "http://localhost:1337",
+          "https://admin.cecsengineer.com",
+        ],
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
